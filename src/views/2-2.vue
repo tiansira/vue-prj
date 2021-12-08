97a44a05ee52915dedfb381c0e0ac079489b40a7
Dingsheng <tiansira@163.com>
2021-12-08 23:53:46 +0800

    2021-12-8_11:53PM

---
+++ src/views/2-2.vue
@@ -0,0 +1,35 @@
+<template>
+ <div>
+     This is test 2-2 view
+ </div>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+
+import axios from 'axios'
+export default {
+  name: 'axios2-2',
+  components: {
+   
+  },
+  created(){
+    axios.get('/data.json',{
+        params:{
+            id:12
+        }
+    }).then((res)=>{
+      console.log(res)
+    })
+
+//post
+    let data ={
+        id:12
+    }
+    axios.post('/post',data).then((res) =>{
+        console.log(res)
+    })
+  }
+}
+</script>
